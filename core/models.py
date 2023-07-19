d7332c6dae818c8844003236f9e80b6354ea015e
sphinx-austin <austinjuniour4@gmail.com>
2023-07-19 18:59:36 +0300

    migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Website(models.Model):
+    name = models.CharField(max_length=256)
+    qr_code = models.ImageField(upload_to='qr_codes', blank=True)
+
+    def __str__(self):
+        return str(self.name)
