95591c1b11865f89f8eac7c33fda33215bb4b186
sphinx-austin <austinjuniour4@gmail.com>
2023-07-19 19:49:43 +0300

    update views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from . models import Website
 
 # Create your views here.
+def home_view(request):
+    name = "Welcome to "
+
+    obj = Website.objects.get(id=3)
+
+    context = {
+        'name': name,
+        'obj' : obj,
+    }
+
+    return render(request, 'core/index.html', context)
