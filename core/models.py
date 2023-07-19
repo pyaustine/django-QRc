bdbec718cb5a27a374d457b2ad99f8d246904c9b
sphinx-austin <austinjuniour4@gmail.com>
2023-07-19 19:26:16 +0300

    improvemenets on qrc generator

--- core/models.py
+++ core/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image, ImageDraw
+
 
 # Create your models here.
 
@@ -8,3 +13,24 @@ class Website(models.Model):
 
     def __str__(self):
         return str(self.name)
+    
+    # overide save method
+    def save(self, *args, **kwargs):
+        # create qrc based on a name field above
+        qrcode_img = qrcode.make(self.name)
+        # construct image based on some params
+        canvas = Image.new('RGB', (290, 290), 'white')
+        # draw
+        draw = ImageDraw.Draw(canvas)
+        canvas.paste(qrcode_img)
+        # create file name
+        fname = f'qr_code-{self.name}.png'
+        # create In-Memeory File Object
+        buffer = BytesIO()
+        canvas.save(buffer, 'PNG')
+        self.qr_code.save(fname, File(buffer), save=False)
+        canvas.close()
+        super().save(*args, **kwargs)
+
+
+    
